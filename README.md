df95bfb149035a23e90a7acfb8336092ad157636
Esteban Borai <estebanborai@outlook.com>
2019-05-18 18:50:38 -0300

    Minor work on MongoDB Authentication

--- README.md
+++ README.md
@@ -26,6 +26,15 @@ docker-compose build
 docker-compose up
 ```
 
+##### SSH into Docker container
+There is a docker exec command that can be used to connect to a container that is already running.
+
+```bash
+docker exec -it <container id> /bin/bash
+```
+
+Get the docker <container id> using `docker container ls` in your terminal, copy the *CONTAINER ID* and change it for *<container id>* in the command above.
+
 ### Debugging
 Here are some steps to setup the environment in order to debug this application using VSCode and Nodemon.
 This project already has the configuration loaded at: `.vscode/launch.json`.
--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ const mongoose = require('mongoose');
 dotenv.config();
 
 const app = express();
-const { SERVER_PORT, MONGO_DB_PORT, MONGO_DB_USERNAME, MONGO_DB_PASSWORD } = process.env;
+const { SERVER_PORT, MONGO_DB_PORT, MONGO_DB_USERNAME, MONGO_DB_PASSWORD, MONGO_DB_DATABASE } = process.env;
 
 app.use(cors());
 app.use(json());
@@ -17,14 +17,28 @@ app.use(morgan('combined'));
 
 app.use(routes);
 
+const mongoDBInstance = `mongodb://mongo:${MONGO_DB_PORT}/${MONGO_DB_DATABASE}`;
+
 mongoose.connect(
-	`mongodb://db:${MONGO_DB_PORT}/simple-rest-crud-sample`, 
+	mongoDBInstance, 
 	{
 		user: MONGO_DB_USERNAME,
 		pass: MONGO_DB_PASSWORD,
 		useNewUrlParser: true
 	}
-);
+).then((res) => {
+	console.log(`Connected to ${mongoDBInstance}`, res); // eslint-disable-line
+}).catch((err) => {
+	if (err) {
+		console.error('Something went wrong at the moment to connect to the database', // eslint-disable-line
+			JSON.stringify({
+				user: MONGO_DB_USERNAME,
+				pass: '???',
+				database: MONGO_DB_DATABASE,
+				connectionString: mongoDBInstance
+			}), err);
+	}
+});
 
 app.listen(SERVER_PORT, () => {
 	console.log(`Running server at http://localhost:${SERVER_PORT}`); // eslint-disable-line
